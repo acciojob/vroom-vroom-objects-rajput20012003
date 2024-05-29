9098bec39863802068075708d3f133ac6309cf6d
Vaishnavi Rajput <162553453+rajput20012003@users.noreply.github.com>
2024-05-29 15:59:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
-/function Car(make, model) {
-  this.make = make;
-  this.model = model;
+// Define the Car constructor function
+function Car(make, model) {
+    this.make = make;
+    this.model = model;
 }
 
+// Add the getMakeModel method to the Car prototype
 Car.prototype.getMakeModel = function() {
-  return `${this.make} ${this.model}`;
+    return `${this.make} ${this.model}`;
 };
 
+// Define the SportsCar constructor function
 function SportsCar(make, model, topSpeed) {
-  Car.call(this, make, model);
-  this.topSpeed = topSpeed;
+    // Call the Car constructor
+    Car.call(this, make, model);
+    this.topSpeed = topSpeed;
 }
 
+// Inherit the Car prototype
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
 
+// Add the getTopSpeed method to the SportsCar prototype
 SportsCar.prototype.getTopSpeed = function() {
-  return this.topSpeed;
+    return this.topSpeed;
 };
 
 // Example usage
